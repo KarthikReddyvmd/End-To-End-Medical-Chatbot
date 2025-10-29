4346bf828f61eb8a59c23da5b02831a3d532abe9
KarthikReddyvmd <Karthikreddy2004v@gmail.com>
2025-10-29 21:58:45 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Generative Ai Project",
+    version="0.1.0",
+    author='Karthik Reddy Vankamaddi',
+    author_email='reddyalpha25@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
